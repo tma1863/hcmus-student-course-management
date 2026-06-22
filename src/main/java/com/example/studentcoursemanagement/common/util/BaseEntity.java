5b84aac7f4404b3c69b095d064a34bf2a5c2fbed
tma1863 <aloratrinh@gmail.com>
2026-06-22 19:43:53 +0700

    feat: Add BaseEntity class for automatic timestamp management

---
+++ src/main/java/com/example/studentcoursemanagement/common/util/BaseEntity.java
@@ -0,0 +1,20 @@
+package com.example.studentcoursemanagement.common.util;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    public LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    public LocalDateTime updatedAt;
+}
